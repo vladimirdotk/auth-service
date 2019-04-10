280a36d844da53163a16c0240282cfd04f0a6c52
wilix-imac-3 <test@test.ru>
2019-04-10 13:18:09 +0300

    Added Role deletion

--- app/routes/role.js
+++ app/routes/role.js
@@ -13,10 +13,21 @@ router.post('/', (req, res) => {
     role
         .save()
         .then((role) => res.json(role))
-        .catch((err) => {
-            res.status = 400
-            res.json(err)
-        });
+        .catch((err) => res.status(400).json(err));
+});
+
+router.delete('/', (req, res) => {
+    Role.findByIdAndRemove({ _id: req.body.id }, (err, role) => {
+        if (err) {
+            return res.status(500).json({ message: 'fail to delete role' });
+        }
+
+        if (role) {
+            return res.json({ message: 'success' })
+        }
+
+        res.status(404).json({ message: 'no role found' })
+    });
 });
 
 module.exports = router;

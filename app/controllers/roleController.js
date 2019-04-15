26578c8dab6eff336cbda88b5df0dc138908e9e1
wilix-imac-3 <test@test.ru>
2019-04-15 15:21:12 +0300

    Moved logic from role router to controller

---
+++ app/controllers/roleController.js
@@ -0,0 +1,38 @@
+const Role = require('./../models/Role');
+
+const getRoles = async (req, res) => {
+    try {
+        res.json(await Role.find());
+    } catch (err) {
+        console.log(`Error getting roles: ${err}`);
+        res.status(500).json({ message: 'failed to get roles' })
+    }
+}
+
+const addRole = async (req, res) => {
+    try {
+        const role = await Role.create({ name: req.body.name });
+        res.status(201).json(role)
+    } catch (err) {
+        console.log(`Error creating a role: ${err}`);
+        res.status(400).json({ message: 'failed to create a role' });
+    }
+}
+
+const deleteRole = async (req, res) => {
+    try {
+        const role = await Role.findByIdAndRemove({ _id: req.params.roleId });
+        return role
+            ? res.json({ message: 'success' })
+            : res.status(404).json({ message: 'no role found' })
+    } catch (err) {
+        console.log(`Error deleting a role: ${err}`);
+        res.status(500).json({ message: 'failed to delete role' });
+    }
+}
+
+module.exports = {
+    getRoles,
+    addRole,
+    deleteRole
+}
--- app/routes/role.js
+++ app/routes/role.js
@@ -1,36 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Role = require('./../models/Role');
+const roleController = require('./../controllers/roleController');
 
-router.get('/', async (req, res) => {
-    try {
-        res.json(await Role.find());
-    } catch (err) {
-        console.log(`Error getting roles: ${err}`);
-        res.status(500).json({ message: 'failed to get roles' })
-    }
-});
-
-router.post('/', async (req, res) => {
-    try {
-        const role = await Role.create({ name: req.body.name });
-        res.status(201).json(role)
-    } catch (err) {
-        console.log(`Error creating a role: ${err}`);
-        res.status(400).json({ message: 'failed to create a role' });
-    }
-});
-
-router.delete('/:roleId', async (req, res) => {
-    try {
-        const role = await Role.findByIdAndRemove({ _id: req.params.roleId });
-        return role
-            ? res.json({ message: 'success' })
-            : res.status(404).json({ message: 'no role found' })
-    } catch (err) {
-        console.log(`Error deleting a role: ${err}`);
-        res.status(500).json({ message: 'failed to delete role' });
-    }
-});
+router.get('/', roleController.getRoles);
+router.post('/', roleController.addRole);
+router.delete('/:roleId', roleController.deleteRole);
 
 module.exports = router;

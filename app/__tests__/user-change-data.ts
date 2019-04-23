1e7551d3808a704ea1e112ad21706ba449ceaf9a
wilix-imac-3 <test@test.ru>
2019-04-23 18:55:03 +0300

    Added user changes data test

---
+++ app/__tests__/user-change-data.ts
@@ -0,0 +1,33 @@
+import app from './../src/app';
+import * as request from 'supertest';
+import url = require('url');
+import { connection } from 'mongoose';
+import { createUser, deleteUser } from './helper';
+
+describe('Change user data', () => {
+    it('changes name', async () => {
+        const user = await createUser();
+        const testName = 'TestName';
+        const response = await request(app)
+            .patch(`/users/${user._id}`)
+            .set('Accept', 'application/json')
+            .send({ name: testName })
+            .expect('Content-Type', /json/)
+            .expect(200);
+        const confirmUrl = JSON.parse(response.text).url;
+        const confirmUrlPath = url.parse(confirmUrl).path;
+        const confirmResponse = await request(app)
+            .get(confirmUrlPath!)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200);
+
+        const result = JSON.parse(confirmResponse.text);
+        expect(result.name).toEqual(testName);
+
+        return deleteUser(user._id);
+    });
+    afterAll(async () => {
+        await connection.close();
+    });
+});

2416c9a6d2265c8280f95db13db50e11de028588
Vladimir <vladimir@kivernik.ru>
2019-05-14 14:19:50 +0300

    Fixed working with docker

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 + Copy `app/.env.dist` to `app/.env`
 + Change `app/.env` variables
-+ Change `docker/mongo/init.js` to create proper database and user/password
++ Run migrations `docker-compose up migrate`
 + Run `docker compose up app` for development or `docker-compose up app-test` for testing
--- app/knexfile.js
+++ app/knexfile.js
@@ -1,12 +1,14 @@
 require('ts-node/register');
+require('dotenv').config();
 
 module.exports = {
     development: {
         client: 'postgresql',
         connection: {
-            database: 'appdb',
-            user:     'app-user',
-            password: 'app-password',
+            host: process.env.POSTGRES_HOST,
+            database: process.env.POSTGRES_DB,
+            user:     process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
         },
         pool: {
             min: 2,
